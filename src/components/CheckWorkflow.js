4b0e3b91538b31fa31d3b273eb53aa4f8f2c0cd5
Dmytro Anikin <dimianni@users.noreply.github.com>
2024-02-01 18:48:25 +0100

    Quickfixes

--- src/components/CheckWorkflow.js
+++ src/components/CheckWorkflow.js
@@ -3,9 +3,9 @@ import Link from 'next/link';
 export default function CheckWorkflow() {
     return (
         <section className='bg-slate-200 rounded-xl p-8'>
-            <h1 className='w-full text-center text-2xl font-bold mb-8'>Admin Use Only</h1>
-            <div className='w-1/2'>
-                <p>See transaction updates <Link rel="noopener noreferrer" target="_blank" className='text-blue-500 underline' href="https://webhook.site/3a4a21e3-02b0-48fd-83bf-3ef66e506a44">here</Link>.</p>
+            <div>
+                <p>See webhooks <Link rel="noopener noreferrer" target="_blank" className='text-blue-500 underline' href="https://webhook.site/3a4a21e3-02b0-48fd-83bf-3ef66e506a44">here</Link>.</p>
+                <p className='font-bold'>Please carry out a transaction using the iFrame below (Main tab).</p>
             </div>
         </section>
     );
--- src/pages/admin.js
+++ src/pages/admin.js
@@ -38,11 +38,16 @@ export default function Admin(){
             <h1 className='w-full text-center text-2xl font-bold mb-8'>Admin Use Only</h1>
             <div className="flex justify-between">
                 <div className='w-2/5'>
-                    <button className='w-full border-none rounded px-4 py-2 text-white font-medium bg-[#888888] shadow-[0_1px_3px_0_rgba(19,57,94,0.4)] hover:bg-[#323416] active:bg-[#0b2a49] cursor-pointer' onClick={handleButtonClick}>Create Checkout Workflow</button>
+                    <button 
+                        className='w-full border-none rounded px-4 py-2 text-white font-medium bg-[#888888] shadow-[0_1px_3px_0_rgba(19,57,94,0.4)] hover:bg-[#323416] active:bg-[#0b2a49] cursor-pointer' 
+                        onClick={handleButtonClick}
+                    >
+                        Create Checkout Workflow
+                    </button>
                 </div>
 
                 <div className='w-1/2'>
-                    <p>See transaction updates <Link rel="noopener noreferrer" target="_blank" className='text-blue-500 underline' href="https://webhook.site/3a4a21e3-02b0-48fd-83bf-3ef66e506a44">here</Link>.</p>
+                    <p>See webhooks <Link rel="noopener noreferrer" target="_blank" className='text-blue-500 underline' href="https://webhook.site/3a4a21e3-02b0-48fd-83bf-3ef66e506a44">here</Link>.</p>
                 </div>
             </div>
             <Toaster position="bottom-center" />
